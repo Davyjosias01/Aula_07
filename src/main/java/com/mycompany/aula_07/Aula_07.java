ab209c97e4dc468faa066af1bc20afed26b2db69
Davy Josias Scheuermann <414773@unoesc.lan>
2025-04-09 19:33:44 -0300

    First commit

---
+++ src/main/java/com/mycompany/aula_07/Aula_07.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aula_07;
+
+/**
+ *
+ * @author 414773
+ */
+public class Aula_07 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
